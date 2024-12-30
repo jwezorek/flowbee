a990432098d7846c0d4f06d879b30ebcb3aa0863
joe wezorek <jwezorek@gmail.com>
2024-12-30 11:44:59 -0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <string>
+#include <print>
+#include <ranges>
+
+namespace r = std::ranges;
+namespace rv = std::ranges::views;
+
+/*------------------------------------------------------------------------------------------------*/
+
+namespace {
+
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
