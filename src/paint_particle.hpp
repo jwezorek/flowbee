b7676c6d18da1051feba1bfc0d9931aef20f78c0
joe wezorek <jwezorek@gmail.com>
2025-01-29 10:24:18 -0800

    broke out pigment

--- src/paint_particle.hpp
+++ src/paint_particle.hpp
@@ -1,39 +1,13 @@
 #pragma once
 
 #include <vector>
-#include <cstdint>
 #include "types.hpp"
-#include "third-party/mixbox.h"
+#include "pigment.hpp"
 #include <unordered_map>
 
 /*------------------------------------------------------------------------------------------------*/
 
 namespace flo {
-
-    struct pigment {
-        mixbox_latent impl;
-
-        bool operator==(const pigment& p) const;
-    };
-
-    pigment operator+(const pigment& lhs, const pigment& rhs);
-    pigment operator*(double k, const pigment& rhs);
-
-    struct hash_pigment {
-        size_t operator()(const pigment& p) const;
-    };
-
-    template <typename T>
-    using pigment_map = std::unordered_map<pigment, T, hash_pigment>;
-
-    pigment rgb_to_pigment(const rgb_color& rgb);
-    pigment rgb_to_pigment(uint8_t r, uint8_t g, uint8_t b);
-
-    void pigment_to_rgb_values(const pigment& p, uint8_t& r, uint8_t& g, uint8_t& b);
-    rgb_color pigment_to_rgb(const pigment& p);
-
-    pigment mix_pigments(const pigment& a, double a_vol, const pigment& b, double b_vol);
-    pigment mix_paint(const pigment_map<double>& pigments);
 
     class paint_particle {
         double volume_;
@@ -50,8 +24,5 @@ namespace flo {
     paint_particle& operator-=(paint_particle& lhs, const paint_particle& rhs);
     paint_particle operator+(const paint_particle& lhs, const paint_particle& rhs);
     paint_particle operator-(const paint_particle& lhs, const paint_particle& rhs);
-    //paint normalize(const paint& p);
 
-    //paint create_paint(int num_colors);
-    //paint make_paint(int num_colors, int index, double val);
 }
---
+++ src/pigment.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include "types.hpp"
+#include "third-party/mixbox.h"
+
+namespace flo {
+
+    struct pigment {
+        mixbox_latent impl;
+
+        bool operator==(const pigment& p) const;
+    };
+
+    pigment operator+(const pigment& lhs, const pigment& rhs);
+    pigment operator*(double k, const pigment& rhs);
+
+    struct hash_pigment {
+        size_t operator()(const pigment& p) const;
+    };
+
+    template <typename T>
+    using pigment_map = std::unordered_map<pigment, T, hash_pigment>;
+
+    pigment rgb_to_pigment(const rgb_color& rgb);
+    pigment rgb_to_pigment(uint8_t r, uint8_t g, uint8_t b);
+
+    void pigment_to_rgb_values(const pigment& p, uint8_t& r, uint8_t& g, uint8_t& b);
+    rgb_color pigment_to_rgb(const pigment& p);
+
+    pigment mix_pigments(const pigment& a, double a_vol, const pigment& b, double b_vol);
+    pigment mix_paint(const pigment_map<double>& pigments);
+
+}
